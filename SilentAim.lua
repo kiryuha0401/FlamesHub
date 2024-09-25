a17555d2d911b9a305686681abe9a31450416248
kiryuha0401 <100902353+kiryuha0401@users.noreply.github.com>
2024-09-25 19:50:23 +0500

    Update SilentAim.lua

--- SilentAim.lua
+++ SilentAim.lua
@@ -1,7 +1,11 @@
 while wait() do
 	for i,v in pairs(game.Workspace:GetChildren()) do
-		if v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") and v.Name == not game.Players.LocalPlayer.Name then
-			v:WaitForChild("HumanoidRootPart").Size = Vector3.new(50,50,50)
+		if v:IsA("Model") and v:FindFirstChild("HumanoidRootPart") then
+			if v.Name == game.Players.LocalPlayer.Name then
+				local s 
+			else
+				v:WaitForChild("HumanoidRootPart").Size = Vector3.new(50,50,50)
+			end
 		end
 	end
 end
